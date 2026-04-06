d65e81a061ca8aa96b2974cd8469ff43d307e643
Omarov <st02230071@gse.cs.msu.ru>
2026-04-06 14:24:48 +0300

    sem

---
+++ 20260406/0/doc/conf.py
@@ -0,0 +1,32 @@
+import sys
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'The project'
+copyright = '2026, Mika'
+author = 'Mika'
+release = '0.0.1'
+
+sys.path.append('..')
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
